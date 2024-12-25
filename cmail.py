a821e6c9d9f52984f88069f47c15e8bf2955c812
VamsiKandala <115799878+VamsiKandala@users.noreply.github.com>
2024-12-25 21:46:37 +0530

    initial commit

--- cmail.py
+++ cmail.py
@@ -2,7 +2,7 @@ import smtplib
 from email.message import EmailMessage
 def sendmail(to,subject,body):
     server=smtplib.SMTP_SSL('smtp.gmail.com',465)
-    server.login('vamsikrishnakandala56@gmail.com','ncqsagdujcjbbsme')
+    server.login('vamsikrishnakandala56@gmail.com','16digitapppasscode')
     msg=EmailMessage()
     msg['From']='vamsikrishnakandala56@gmail.com'
     msg['To']=to
